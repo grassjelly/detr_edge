622a4ce5c983883320460b819f295b2e2a5556a3
Juan Jimeno <jimenojmm@gmail.com>
2022-04-14 22:22:51 +0800

    added model inputs

--- detr_config.py
+++ detr_config.py
@@ -11,8 +11,15 @@ class DetrOnnxConfig(OnnxSeq2SeqConfigWithPast):
     def inputs(self) -> Mapping[str, Mapping[int, str]]:
         return OrderedDict(
             [
-                ("input_ids", {0: "batch", 1: "sequence"}),
-                ("attention_mask", {0: "batch", 1: "sequence"}),
+                ("pixel_values", {0: "batch_size", 1: "num_channels", 2: "height", 3: "width"}),
+                ("pixel_mask", {0: "batch_size", 1: "height", 2: "width"}),
+                # ("decoder_attention_mask", {0: "batch_size", 1: "num_queries"}),
+                # ("encoder_outputs", {0: "last_hidden_state", 1: "last_hidden_states"}),
+                # ("inputs_embeds", {0: "batch", 1: "height", 2: "width"}),
+                # ("decoder_inputs_embeds", {0: "batch", 1: "height", 2: "width"}),
+                # ("output_attentions", {0: "batch", 1: "height", 2: "width"}),
+                # ("output_hidden_states", {0: "batch", 1: "height", 2: "width"}),
+                # ("return_dict", {0: "batch", 1: "height", 2: "width"}),
             ]
         )
 
